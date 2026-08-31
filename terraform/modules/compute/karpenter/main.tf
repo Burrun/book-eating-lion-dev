2986428fa458e518831836cf314f32c6714337b0
burrun <dhrjsdhrj0@gmail.com>
2026-08-31 14:32:40 +0900

    fix(infra): Karpenter 및 EKS 노드 IAM Role에 CloudWatch 및 X-Ray 권한 추가

--- terraform/modules/compute/karpenter/main.tf
+++ terraform/modules/compute/karpenter/main.tf
@@ -18,6 +18,8 @@ resource "aws_iam_role_policy_attachment" "node" {
     "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
     "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
+    "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy",
+    "arn:aws:iam::aws:policy/AWSXrayWriteOnlyAccess",
   ])
   role       = aws_iam_role.node.name
   policy_arn = each.value
